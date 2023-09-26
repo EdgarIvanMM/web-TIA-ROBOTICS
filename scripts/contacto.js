d51d6d28ce82949df0513d820555aeec62529ace
EdgarIvanMM <edgar.ivanmm97@gmail.com>
2023-09-26 16:20:30 -0600

    26.09.23

--- scripts/contacto.js
+++ scripts/contacto.js
@@ -18,3 +18,14 @@
     // Oculta la información de contacto al hacer clic en la tacha
     info.style.display = 'none';
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const menuToggle = document.getElementById('menu-toggle');
+  const nav = document.querySelector('nav');
+
+  // Agrega un event listener al botón de la hamburguesa
+  menuToggle.addEventListener('click', function () {
+    // Toggle la clase 'show' en la navegación para mostrar u ocultar
+    nav.classList.toggle('show');
+  });
+});
